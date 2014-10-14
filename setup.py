e7d30b675d85da3fef58de2c7342610dc9566a06
Dale Jung <dale@dalejung.com>
2014-10-14 18:53:05 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+DISTNAME='naginpy'
+FULLVERSION='0.1-dev'
+
+setup(
+    name=DISTNAME,
+    version=FULLVERSION,
+    packages=['naginpy'],
+    url='http://github.com/dalejung/naginpy/',
+    license='BSD',
+    author='Dale Jung',
+    author_email='dale@dalejung.com',
+)
