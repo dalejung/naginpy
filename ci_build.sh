bbc2b380a278a129449ee170fb22efa7f687b6e8
Dale Jung <dale@dalejung.com>
2018-08-19 14:14:12 -0400

    ED doesn't need dev pandas anymore

--- ci_build.sh
+++ ci_build.sh
@@ -4,7 +4,7 @@ pip install --upgrade pip
 pip install nose
 pip install cython
 pip install numpy
-pip install git+https://github.com/pandas-dev/pandas.git#egg=pandas
+pip install pandas
 pip install git+https://github.com/dalejung/asttools
 pip install -e git+https://github.com/dalejung/earthdragon#egg=earthdragon
 pip install .
