0c0868676f6b30693ec558cc16b5169aad5d6d54
Dale Jung <dale@dalejung.com>
2018-08-19 04:02:25 -0400

    ci update

--- ci_build.sh
+++ ci_build.sh
@@ -2,8 +2,9 @@
 
 pip install --upgrade pip
 pip install nose
+pip install cython
 pip install numpy
-pip install pandas
+pip install git+https://github.com/pandas-dev/pandas.git#egg=pandas
 pip install git+https://github.com/dalejung/asttools
 pip install git+https://github.com/dalejung/earthdragon
 pip install .
