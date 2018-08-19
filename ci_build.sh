33dae3159fdac84c3c0412405a0352e849f3b4b2
Dale Jung <dale@dalejung.com>
2018-08-19 13:29:16 -0400

    ed

--- ci_build.sh
+++ ci_build.sh
@@ -6,7 +6,7 @@ pip install cython
 pip install numpy
 pip install git+https://github.com/pandas-dev/pandas.git#egg=pandas
 pip install git+https://github.com/dalejung/asttools
-pip install git+https://github.com/dalejung/earthdragon
+pip install -e git+https://github.com/dalejung/earthdragon#egg=earthdragon
 pip install .
 
 pip freeze
